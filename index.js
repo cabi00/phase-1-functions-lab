bf9f96d100f76954f5a2db51d20b10e1795238a4
vanessa garcia <venessag0908@gmail.com>
2023-03-24 23:13:27 -0600

    functions lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(x) {
+   return Math.abs(42-x);
+}
+
+function distanceFromHqInFeet(f){
+    return distanceFromHqInBlocks(f)*264;
+}   
+
+function distanceTravelledInFeet(x,y){
+    return (Math.abs(x-y)*264);
+}
+
+function calculatesFarePrice(x,y){
+    let z = (Math.abs(x-y)*264);
+    if(z<400){
+        return 0;
+    }
+    else if (z < 2000){
+        return ((z-400)*0.02);
+    }
+    else if (z < 2500){
+        return 25;
+    }
+    else {
+        return 'cannot travel that far';
+    }
+}
